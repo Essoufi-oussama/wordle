8f2c68c4028c32b1e31725bc30a59711be36d5f0
Essoufi Oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2024-03-06 23:24:19 +0100

    created js and finished i am sure years for now i would look at this code and realise it is very inneficient but hey progress is progress

---
+++ wordle.js
@@ -0,0 +1,100 @@
+const word_url = "https://words.dev-apis.com/word-of-the-day"
+let word = "";
+let id = 1;
+
+const getWord = async () => {
+    const promise = await  fetch(word_url);
+    const processedResponse = await promise.json();
+    word = processedResponse.word;
+}
+
+const validateWord = async (wordToCheck, divArray) =>{
+    // the two lines below looks weird but i did so user will not be able to click when he does post request
+    document.removeEventListener("keydown", keyClick);
+    document.addEventListener("keydown", keyClick);
+    const wordTosubmit = {
+        word: wordToCheck
+    };
+    
+    const promise = await fetch("https://words.dev-apis.com/validate-word", {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify(wordTosubmit)
+            });
+
+    const processedResponse = await promise.json();
+        if (processedResponse.validWord){
+            if (wordToCheck === word){
+                alert("you win!");
+                for(let i = 0; i < divArray.length; i++ ){
+                    divArray[i].style.cssText = "background-color: green; color: white;"
+                }
+                document.removeEventListener("keydown", keyClick);
+            } else {
+                for(let i= 0; i < divArray.length; i++){
+                    if(wordToCheck[i] === word[i] ){
+                        divArray[i].style.cssText = "background-color: green;"
+                    }else if(word.includes(wordToCheck[i])){
+                        divArray[i].style.cssText = "background-color: yellow;"
+                    }else{
+                        divArray[i].style.cssText = "background-color: gray;"
+                    }
+                }
+                id = id + 1;
+                if(id === 7){
+                    document.removeEventListener("keydown", keyClick);
+                    alert('You lose!');
+                }
+            }
+        } else {
+            alert("Invalid Word");       
+        }       
+}
+
+ window.onload = getWord();
+
+ function keyClick (event) {
+    //event gives KeyboardEvent {isTrusted: true, key: '4', code: 'Digit4', location: 0, ctrlKey: false, …}
+    // populate th row
+    if (!(event.code.includes("Key") || event.code === 'Backspace' || event.code === 'Enter')){
+        return 0;
+    }
+    const test = document.getElementById(id).children;
+
+    // will check if the pressed key is letter and update 
+
+    if (event.code.includes("Key")){
+        // i wanted to not do loop if the last div is populated
+        if (test[4].innerText !== ''){
+            test[4].innerText = event.key;
+            return 0;
+        }
+
+        for(let i = 0 ; i < test.length; i++){
+            if (test[i].innerHTML===''){
+                test[i].innerHTML = event.key;
+                break;
+            }
+        }
+     } else if (event.code === 'Backspace'){
+        for(let i = 0; i < test.length; i++){
+            if( i=== 4){
+                test[4].innerText = '';
+                break;
+            } else{
+                if (test[i + 1].innerText ==='' && test[i].innerText !== ''){
+                    test[i].innerText = '';
+                }
+            }
+        }
+     } else if (event.code == 'Enter'){
+        // send post request only if last letter of div is right
+        if (test[4].innerText !== ''){
+            const wordTosubmit = test[0].innerText + test[1].innerText + test[2].innerText + test[3].innerText + test[4].innerText;
+            validateWord(wordTosubmit.toLowerCase(), test);
+        }
+     }
+}
+document.addEventListener("keydown", keyClick);
